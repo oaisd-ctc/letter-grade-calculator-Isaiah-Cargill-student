b4a92fcd5c6e16e8f668b23d2820ac0069f3904f
Isaiah-Cargill-student <isca71b@student.oaisd.org>
2024-10-22 13:36:46 +0000

    i put the "if" for every number 1 to 100

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+public class Program
+
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter a score from 1 to 100");
+        
+        int score = int.Parse (Console.ReadLine());
+      if (score >= 100 )
+      {
+          Console.WriteLine("A+");
+
+          Console.WriteLine("Wow! 100…That's impressive!");
+      }
+
+        else if (score >= 90)
+        {
+         Console.WriteLine("A");
+
+         Console.WriteLine("Very nice!");
+        }
+
+        else if (score >= 80)
+        {
+           Console.WriteLine("B"); 
+           Console.WriteLine("Keep it up!");
+        }
+        
+        else if (score >= 70)
+        {
+          Console.WriteLine("C");
+          Console.WriteLine("You got this, keep working!");  
+        }
+
+        else if (score >= 60)
+        {
+        Console.WriteLine("D");
+        Console.WriteLine(" Keep trying! ");
+      }
+      else 
+      {
+        Console.WriteLine("E");
+        Console.WriteLine("  Don't give up! ");
+      }
+    }
+
+}
